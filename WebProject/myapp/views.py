ae8660e40937b7bb91c1cfb1e3d73705538f453f
Nur Bengisu <bengisu.cam@gmail.com>
2020-05-10 22:59:28 +0300

    Update views.py

--- WebProject/myapp/views.py
+++ WebProject/myapp/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import SimpleForm
 
 # Create your views here.
 
+
+def create_form_view(request):
+    form = SimpleForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    return render(request, "myapp/forms.html", {'form': form})
+
+
+
 def hi(request):
-    return render(request, "myapp/display.html")
+    return render(request, "myapp/base.html")
     # return HttpResponse("<h1>Hello<h1>")
