7b53a1353c5912e0c8330e1fda7f6fb0c934ee5a
Nur Bengisu <bengisu.cam@gmail.com>
2020-05-12 01:36:55 +0300

    Update settings.py

--- WebProject/WebProject/settings.py
+++ WebProject/WebProject/settings.py
@@ -121,5 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, 'static'),
+    'WebProject/myapp/static',  # Here you tell django to look for a folder
+]
+
 STATIC_URL = '/static/'
+
+
 
