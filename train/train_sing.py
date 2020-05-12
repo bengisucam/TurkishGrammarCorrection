35deb4fb8aabb457e9c2118fb02275e03f4bb145
Furkan Çağlayan <furkancaglayan15@hotmail.com>
2020-05-12 21:21:46 +0300

    Update train_sing.py

--- train/train_sing.py
+++ train/train_sing.py
@@ -2,6 +2,8 @@ import argparse
 import logging
 import sys
 
+sys.path.append("/home/grammar/deneme/TurkishGrammarCorrection")
+
 from train.train_seq import train
 
 if __name__ == "__main__":
@@ -11,7 +13,6 @@ if __name__ == "__main__":
     # parser.add_argument('--save', action='store', dest='save',
     #                     help='Save dir')
     # opt = parser.parse_args()
-    sys.path.append("/home/grammar/deneme/TurkishGrammarCorrection")
     logging.basicConfig(level=logging.INFO)
     config_path = 'Configuration/config.yaml'
     train(config_path, './Experiments', 'Experiment01')
