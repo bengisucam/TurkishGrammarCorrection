84135f4198e4c852adaa7c336283a042cdd9e935
Nur Bengisu <bengisu.cam@gmail.com>
2020-06-12 23:57:36 +0300

    encoder format back to word rnn

--- seq2seq/models/EncoderRNN.py
+++ seq2seq/models/EncoderRNN.py
@@ -40,11 +40,16 @@ class EncoderRNN(BaseRNN):
 
     def __init__(self, vocab_size, max_len, hidden_size,
                  input_dropout_p=0, dropout_p=0,
-                 n_layers=1, bidirectional=False, rnn_cell='gru', variable_lengths=False):
+                 n_layers=1, bidirectional=False, rnn_cell='gru', variable_lengths=False,
+                 embedding=None, update_embedding=True):
         super(EncoderRNN, self).__init__(vocab_size, max_len, hidden_size,
                                          input_dropout_p, dropout_p, n_layers, rnn_cell)
 
         self.variable_lengths = variable_lengths
+        self.embedding = nn.Embedding(vocab_size, hidden_size)
+        if embedding is not None:
+            self.embedding.weight = nn.Parameter(embedding)
+        self.embedding.weight.requires_grad = update_embedding
         self.rnn = self.rnn_cell(hidden_size, hidden_size, n_layers,
                                  batch_first=True, bidirectional=bidirectional, dropout=dropout_p)
 
@@ -62,7 +67,8 @@ class EncoderRNN(BaseRNN):
             - **hidden** (num_layers * num_directions, batch, hidden_size): variable containing the features in the hidden state h
             :param embeddings:
         """
-        embedded = self.input_dropout(embeddings)
+        embedded = self.embedding(input_var)
+        embedded = self.input_dropout(embedded)
         if self.variable_lengths:
             embedded = nn.utils.rnn.pack_padded_sequence(embedded, input_lengths, batch_first=True)
         output, hidden = self.rnn(embedded)
