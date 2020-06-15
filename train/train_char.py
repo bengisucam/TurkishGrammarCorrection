252f3cde16551f396a581fc65a278af03b2c3bc5
Furkan Çağlayan <furkancaglayan15@hotmail.com>
2020-06-15 14:20:04 +0300

    update to configuration file. Replace print with logging.

--- train/train_char.py
+++ train/train_char.py
@@ -7,13 +7,11 @@ import torchtext
 from torch.optim.lr_scheduler import StepLR
 from torchtext.vocab import FastText
 
-
 import sys
+
 sys.path.append("/content/drive/My Drive/GrammarCorr/TurkishGrammarCorrection/")
 
 from train.predict import predict
-
-
 
 from seq2seq.models.bilstm import BiLSTM
 from seq2seq.dataset import SourceField, TargetField
@@ -23,6 +21,7 @@ from seq2seq.optim import Optimizer
 from seq2seq.trainer import SupervisedTrainer
 from train.train_seq import parse_yaml
 
+
 def tokenize(sentence):
     return list(" ".join(sentence.split()))
 
@@ -31,6 +30,7 @@ logging.basicConfig(level=logging.INFO)
 
 config = parse_yaml('Configuration/config.yaml')
 
+logging.info(config)
 max_length = config['dataset']['max_length']
 
 src = SourceField()
@@ -50,20 +50,21 @@ ___, __, _ = torchtext.data.TabularDataset.splits(
 
 embeddings = None
 if bool(config['dataset']['word_embeddings']['use']):
-    embeddings = FastText(language='tr',cache='vectors')
+    embeddings = FastText(language='tr', cache='vectors')
 max_vocab_size = int(config['dataset']['max_vocab'])
 src.build_vocab(train, max_size=max_vocab_size, vectors=embeddings)
 tgt.build_vocab(train, max_size=max_vocab_size, vectors=embeddings)
 src.vocab.extend(tgt.vocab)
 
 chars.build_vocab(___, max_size=max_vocab_size)
-print('- src vocab size: {}'.format(len(src.vocab)))
-print('- tgt vocab size: {}'.format(len(tgt.vocab)))
-print('- chars vocab size: {}'.format(len(chars.vocab)))
+logging.info(f'Train size: {len(train)}\nTest size: {len(test)}\nEval size: {len(dev)}')
+logging.info(f'- src vocab size: {len(src.vocab)}')
+logging.info(f'- tgt vocab size: {len(tgt.vocab)}')
+logging.info(f'- chars vocab size: {len(chars.vocab)}')
 input_vocab = src.vocab
 output_vocab = tgt.vocab
 
-print(chars.vocab.stoi)
+logging.info(chars.vocab.stoi)
 
 device = config['model']['device']
 if not torch.cuda.is_available():
@@ -75,16 +76,18 @@ pad = tgt.vocab.stoi[tgt.pad_token]
 loss = NLLLoss(weight, pad)
 if device == 'cuda' and torch.cuda.is_available():
     loss.cuda()
-print('- creating encoder-decoder')
+logging.info('- creating encoder-decoder')
 bidirectional = bool(config['model']['bidirectional'])
 
 hidden_size = config['model']['encoder_lstm_out_decoder_in']
 bilstm_hidden_size = int(hidden_size / 2)
 
-bilstm = BiLSTM(bilstm_hidden_size, config['model']['char_embedding_size'], int(config['model']['n_layers']), chars, src)
+bilstm = BiLSTM(bilstm_hidden_size, config['model']['char_embedding_size'], int(config['model']['n_layers']), chars,
+                src)
 encoder = EncoderRNN(len(src.vocab),
                      max_len=max_length,
-                     embedding_total_size=int(config['model']['encoder_lstm_out_decoder_in'])+ int(config['model']['word_embedding_size']),
+                     embedding_total_size=int(config['model']['encoder_lstm_out_decoder_in']) + int(
+                         config['model']['word_embedding_size']),
                      hidden_size=hidden_size,
                      n_layers=int(config['model']['n_layers']),
                      rnn_cell=config['model']['rnn_cell'],
@@ -95,7 +98,8 @@ encoder = EncoderRNN(len(src.vocab),
                      weights=src.vocab.vectors,
                      update_embedding=bool(config['dataset']['word_embeddings']['update']))
 decoder = DecoderRNN(len(tgt.vocab), max_length, hidden_size * 2 if bidirectional else hidden_size,
-                     embedding_total_size=int(config['model']['encoder_lstm_out_decoder_in'])+ int(config['model']['word_embedding_size']),
+                     embedding_total_size=int(config['model']['encoder_lstm_out_decoder_in']) + int(
+                         config['model']['word_embedding_size']),
                      n_layers=int(config['model']['n_layers']),
                      rnn_cell=str(config['model']['rnn_cell']),
                      dropout_p=float(config['model']['dropout_output']),
@@ -116,14 +120,13 @@ for param in seq2seq.parameters():
 for param in bilstm.parameters():
     param.data.uniform_(-0.08, 0.08)
 
-
 lr = config['train']['lr']
-optimizer = Optimizer(torch.optim.Adam(list(seq2seq.parameters())+list(bilstm.parameters()), lr=lr), max_grad_norm=5)
+optimizer = Optimizer(torch.optim.Adam(list(seq2seq.parameters()) + list(bilstm.parameters()), lr=lr), max_grad_norm=5)
 if config['model']['scheduler']['enabled']:
     scheduler = StepLR(optimizer.optimizer, config['model']['scheduler']['rate'])
     optimizer.set_scheduler(scheduler)
-print('- starting training')
-print(f'- device {device}\n')
+logging.info('- starting training')
+logging.info(f'- device {device}\n')
 
 t = SupervisedTrainer(loss=loss,
                       batch_size=int(config['train']['batch_size']),
@@ -131,19 +134,19 @@ t = SupervisedTrainer(loss=loss,
                       early_stop_threshold=int(config['train']['early_stop_threshold']),
                       save_name="save_name",
                       checkpoint_every=int(config['train']['checkpoint_every']))
-print(
+logging.info(
     f'Allocated GPU Mem: {torch.cuda.memory_allocated(0)} - Cached Mem: {torch.cuda.memory_cached(0)} - Free Mem: {torch.cuda.get_device_properties(0).total_memory - torch.cuda.memory_allocated(0)}')
 
-seq2seq,path = t.train(seq2seq,bilstm, train, dev_data=dev, test_data=test,
-                  num_epochs=config['train']['epoch'],
-                  optimizer=optimizer,
-                  teacher_forcing_ratio=config['train']['teacher_forcing_ratio'], deviceName=device)
+seq2seq, path = t.train(seq2seq, bilstm, train, dev_data=dev, test_data=test,
+                        num_epochs=config['train']['epoch'],
+                        optimizer=optimizer,
+                        teacher_forcing_ratio=config['train']['teacher_forcing_ratio'], deviceName=device)
 logging.shutdown()
 if device == 'cuda':
     torch.cuda.empty_cache()
-print("- emptying cuda cache")
+logging.info("- emptying cuda cache")
 time.sleep(5)
 
-test_path=os.path.join(config['dataset']['path'],config['dataset']['test'])
-predict(seq2seq,bilstm,input_vocab, output_vocab,test_path , path,
-        max_len=max_length, n=500,device='cuda')
+test_path = os.path.join(config['dataset']['path'], config['dataset']['test'])
+predict(seq2seq, bilstm, input_vocab, output_vocab, test_path, path,
+        max_len=max_length, n=500, device='cuda')
