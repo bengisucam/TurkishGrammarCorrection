e09e1834c788d9b6710f02030f32bd6a01296bda
Nur Bengisu <bengisu.cam@gmail.com>
2020-06-19 11:16:50 +0300

    POST method returns None solved

--- WebProject/myapp/views.py
+++ WebProject/myapp/views.py
@@ -13,15 +13,18 @@ def form_view(request):
 
 def output_text_view(request, context):
     output_text = context['input_text']
-    print(output_text)
+    # print(output_text)
     request.GET.get(output_text)
     return render(request, 'myapp/forms.html', context)
 
 
 def submit_text_view(request):
-    input_text = request.POST.get("input_text")
+    text = ''
+    input_text = request.POST.get("in_text")
+    # text += input_text
     print(input_text)
     context = {'input_text': input_text}
     return output_text_view(request, context)
+
 
 
