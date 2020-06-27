6530f84a98885c1d3a75bf662cc1d8c08d88ae7a
Nur Bengisu <bengisu.cam@gmail.com>
2020-06-27 21:33:17 +0300

    render text functionality

--- WebProject/myapp/views.py
+++ WebProject/myapp/views.py
@@ -3,15 +3,18 @@ from django.http import HttpResponse
 
 
 def home_view(request):
+    print("in home_view")
     return render(request, "myapp/base.html")
     # return HttpResponse("<h1>Hello<h1>")
 
 
 def form_view(request):
+    print("in form_view")
     return render(request, "myapp/forms.html")
 
 
 def output_text_view(request, context):
+    print("in output_view")
     output_text = context['input_text']
     # print(output_text)
     request.GET.get(output_text)
@@ -19,12 +22,12 @@ def output_text_view(request, context):
 
 
 def submit_text_view(request):
+    print("in submit_view")
     text = ''
     input_text = request.POST.get("in_text")
     # text += input_text
     print(input_text)
     context = {'input_text': input_text}
     return output_text_view(request, context)
-
 
 
