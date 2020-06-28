472912882e124327837f3311dd8eacc9488eaad7
Furkan Çağlayan <furkancaglayan15@hotmail.com>
2020-06-28 11:53:36 +0300

    fixes.

--- zemberek_python/create_dataset.py
+++ zemberek_python/create_dataset.py
@@ -3,41 +3,39 @@ import logging
 from zemberek_python.base import ZemberekPython
 from zemberek_python.rule import GrammarRule
 
-
 logging.basicConfig(level=logging.INFO)
 
 ZEMBEREK_PATH = '../data/zemberek/zemberek-full.jar'
 zemberek = ZemberekPython(ZEMBEREK_PATH)
 zemberek = zemberek.startJVM().CreateTokenizer().CreateTurkishMorphology().CreateSpellChecker()
 
-
 frequentlyMiswrittenWords = ['birçok', 'poğaça', 'yalnız', 'birkaç', 'yanlış']
 miswrittenStates = ['bir çok', 'poğça', 'yanlız', 'bir kaç', 'yalnış']
 
-soru_ekleri=['mıyım','miyim','muyum','müyüm',
-             'musun','müsün','mısın','misin',
-             'mu','mü','mı','mi',
-             'muyuz','müyüz','mıyız','miyiz',
-             'musunuz','müsünüz','mısınız','misiniz',
-             ]
-soru_ekleri_Target=['-'+a for a in soru_ekleri]
+soru_ekleri = ['mıyım', 'miyim', 'muyum', 'müyüm',
+               'musun', 'müsün', 'mısın', 'misin',
+               'mu', 'mü', 'mı', 'mi',
+               'muyuz', 'müyüz', 'mıyız', 'miyiz',
+               'musunuz', 'müsünüz', 'mısınız', 'misiniz',
+               ]
+soru_ekleri_Target = ['-' + a for a in soru_ekleri]
 Rules = \
     [
         GrammarRule().IfContentIs(['şey']).ChangeTo(['-şey']).AddDescription("Şey'leri ayır"),
         GrammarRule().IfContentIs(frequentlyMiswrittenWords).ChangeTo(miswrittenStates).AddDescription(
             "Sıklıkla hata yapılan kelimeler"),
-        GrammarRule().IfContentIs(['?', '!']).IfPrimaryPosIs('Punc').ChangeTo(['-', '-']).WithChance(1).AddDescription(
+        GrammarRule().IfContentIs(['?', '!', '\x92']).IfPrimaryPosIs('Punc').ChangeTo(['-', '-', '-']).WithChance(
+            1).AddDescription(
             "Soru ve ünlem işaretlerini sil"),
         GrammarRule().IfContentIs([',', '.']).IfPrimaryPosIs('Punc').ChangeTo(['-', '-']).WithChance(
             1.0).AddDescription("Nokta ve virgül sil"),
 
-
         GrammarRule().IfContentIs(soru_ekleri).
             ChangeTo(soru_ekleri_Target)
             .AddDescription("Ayri yazılan de/da'yi birlestir"),
-        # GrammarRule().IfUpperCase().Lower().AddDescription("Büyük harfleri küçült"),
-        # GrammarRule().IfSecondaryPosIs('ProperNoun').IfUpperCase().Remove('\'').AddDescription(
-        #     "Kesme işaretlerini sil"),
+        GrammarRule().IfUpperCase().Lower().AddDescription("Büyük harfleri küçült"),
+        GrammarRule().IfSecondaryPosIs('ProperNoun').IfUpperCase().Remove('\'').AddDescription(
+            "Kesme işaretlerini sil"),
         GrammarRule().IfContentIs(['de', 'da']).IfPrimaryPosIs('Conj').ChangeTo(['-de', '-da'])
             .AddDescription("Ayri yazılan de/da'yi birlestir"),
         GrammarRule().IfContentIs(['ki']).IfPrimaryPosIs('Conj').ChangeTo(['-ki'])
@@ -53,6 +51,8 @@ Rules = \
     ]
 zemberek.AddRules(Rules)
 
-zemberek = zemberek.open(path='../data/target.txt').process(min_tok=2, max_tok=15).write('./data/source.txt', mode='x')
+for file in ['xaa.txt', 'xab.txt', 'xac.txt', 'xad.txt']:
+    zemberek.open(path=f'../data/newscor/{file}').process(min_tok=1, max_tok=15).write(f'../data/newscor_processed/source_{file}', mode='x',
+                                                                                       write_target=True,target_path=f'../data/newscor_processed/target_{file}')
 
 zemberek.endJVM()
