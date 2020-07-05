2e20c1b37287814eb55e3a7e25ba6f2e4485287e
Furkan Çağlayan <furkancaglayan15@hotmail.com>
2020-07-05 11:00:09 +0300

    Update views.py

--- WebProject/grammarcorrection/views.py
+++ WebProject/grammarcorrection/views.py
@@ -15,34 +15,37 @@ def output_text_view(request, context):
     inp = context['input_text']
     # out = run([sys.executable, './test.py', inp],
     #           shell=False, stdout=PIPE)
-    token_analysis = MyappConfig.zemberek.Tokenize(MyappConfig.zemberek.NormalizeSentence(inp.strip()))
-    tokList = [str(tok.content).lower() for tok in token_analysis]
-    print(tokList)
-    output=MyappConfig.predictor.predict(tokList)
-    token_analysis = MyappConfig.zemberek.Tokenize(' '.join(output[:-1]).strip())
-    tokList.clear()
-    last_was_punc=False
-    for token in token_analysis:
-        analysis = MyappConfig.zemberek.AnalyzeWord(token.content)
-        try:
-            if str(analysis[0].getDictionaryItem().secondaryPos) =='ProperNoun':
-                tokList.append(str(token.content)[0].upper()+str(token.content)[1:])
-            elif str(analysis[0].getDictionaryItem().secondaryPos) =='Abbreviation  ':
-                tokList.append(str(token.content).upper())
-            else:
+    if inp == '' or inp is None:
+        output = 'Lütfen geçerli bir giriş yapın!'
+    else:
+        token_analysis = MyappConfig.zemberek.Tokenize(MyappConfig.zemberek.NormalizeSentence(inp.strip()))
+        tokList = [str(tok.content).lower() for tok in token_analysis]
+        print(tokList)
+        output=MyappConfig.predictor.predict(tokList)
+        token_analysis = MyappConfig.zemberek.Tokenize(' '.join(output[:-1]).strip())
+        tokList.clear()
+        last_was_punc=False
+        for token in token_analysis:
+            analysis = MyappConfig.zemberek.AnalyzeWord(token.content)
+            try:
+                if str(analysis[0].getDictionaryItem().secondaryPos) =='ProperNoun':
+                    tokList.append(str(token.content)[0].upper()+str(token.content)[1:])
+                elif str(analysis[0].getDictionaryItem().secondaryPos) =='Abbreviation  ':
+                    tokList.append(str(token.content).upper())
+                else:
+                    tokList.append(str(token.content))
+            except:
                 tokList.append(str(token.content))
-        except:
-            tokList.append(str(token.content))
 
-        if not last_was_punc:
-            if tokList[-1] in ['.',':','?','!']:
-                last_was_punc=True
-        else:
-            tokList[-1] = str(tokList[-1])[0].upper()+str(tokList[-1])[1:]
-            last_was_punc=False
+            if not last_was_punc:
+                if tokList[-1] in ['.',':','?','!']:
+                    last_was_punc=True
+            else:
+                tokList[-1] = str(tokList[-1])[0].upper()+str(tokList[-1])[1:]
+                last_was_punc=False
 
-    output = ' '.join(tokList)
-    output = str(output[0].upper()) + str(output)[1:]
+        output = ' '.join(tokList)
+        output = str(output[0].upper()) + str(output)[1:]
     # out = run([sys.executable, '../train/train_funcs.py', inp],
     #           shell=False, stdout=PIPE)
     # output = unicode(out.stdout, "utf-8")
